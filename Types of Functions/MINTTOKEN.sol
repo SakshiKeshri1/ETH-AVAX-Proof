4a8ad89cacd86d7601f3f03e8cb7904788f6a22e
SakshiKeshri1 <keshrisakshi073@gmail.com>
2024-06-23 21:23:39 +0530

    Update MINTTOKEN.sol

--- Types of Functions/MINTTOKEN.sol
+++ Types of Functions/MINTTOKEN.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.25;
 
-// import OpenZeppli's ERC20 and ownable contracts
+// import OpenZepplin's ERC20 and ownable contracts
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MINTTOKEN is ERC20 {
